ca16bc70520cc0129c3fb7dfb606cb885298609f
Dan <vareladanmarj@gmail.com>
2022-12-06 07:16:08 +0800

    update tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
+  setup do
+    @user = users :one
+  end
+
   test "should not save when name is empty" do
-    category = Category.new
+    category = Category.new user: @user
     assert_not category.save
   end
 
   test "name should be atleast 5 characters long" do
-    category = Category.new name: "aaa"
+    category = Category.new name: "aaa", user: @user
     assert_not category.save
   end
 end
