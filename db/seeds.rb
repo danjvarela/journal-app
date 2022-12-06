a713f8d64a22e829eb27ed7fef632f7cbd690383
Dan <vareladanmarj@gmail.com>
2022-12-06 10:29:30 +0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-["General", "Studies", "Work_Stuff", "Important"].each do |category_name|
-  category = Category.create name: category_name
-  10.times { |n| category.tasks.create name: "Task #{n + 1}", description: "Task #{n + 1} description", deadline: Time.zone.now + [45.minutes, 1.days, 4.days].sample }
+["user1@example", "user2@example"].each { |email| User.create email: email, password: "password" }
+
+["General", "Studies", "Work Stuff", "Important"].each do |category_name|
+  category = User.first.categories.create name: category_name
+  3.times { |n| category.tasks.create name: "Task #{n + 1}", description: "Task #{n + 1} description", deadline: Time.zone.now + [45.minutes, 1.days, 4.days].sample }
 end
