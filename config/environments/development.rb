2ad11f10f0d41f67a831912de0b9fd7f595c2f69
Dan <vareladanmarj@gmail.com>
2022-12-06 21:02:57 +0800

    add ngrok host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "d3a1-158-62-27-13.ap.ngrok.io"
 end
